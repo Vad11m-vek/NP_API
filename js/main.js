3904ee7fc66e311c72068517ceb69d7e6ead36e8
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-05-16 22:43:36 +0300

    refresh

--- js/main.js
+++ js/main.js
@@ -1,25 +1,18 @@
-fetch('https://api.novaposhta.ua/v2.0/json/')
+$(function () {
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://api.novaposhta.ua/v2.0/json/",
+		"method": "POST",
+		"headers": {
+			"content-type": "application/json",
 
-	.then((response) => {
-		return response.json();
-	})
-
-	.then((data) => {
-		console.log(data);
-	});
-
-let NPKey = {
-
-	"apiKey": "bf9184305646fc07dfee4e2c2f6826e7",
-	"modelName": "InternetDocument",
-	"calledMethod": "getDocumentList",
-	"methodProperties": {
-
-		"DateTimeFrom": "01.01.2020",
-		"DateTimeTo": "24.06.2020",
-		"Page": "1",
-		"GetFullList": "1"
-
+		},
+		"processData": false,
+		"data": "{\r\n\"apiKey\": \"\",\r\n \"modelName\": \"Address\",\r\n \"calledMethod\": \"searchSettlements\",\r\n \"methodProperties\": {\r\n \"CityName\": \"васильевка\",\r\n \"Limit\": 5\r\n }\r\n}"
 	}
 
-}
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+});
