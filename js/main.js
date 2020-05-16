d70a4ed514cd3a7a82529a28ba1ff7899a5b0bed
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-05-16 22:03:34 +0300

    come back

--- js/main.js
+++ js/main.js
@@ -1,44 +1,25 @@
-// fetch('https://api.novaposhta.ua/v2.0/json/')
+fetch('https://api.novaposhta.ua/v2.0/json/')
 
-// 	.then((response) => {
-// 		return response.json();
-// 	})
+	.then((response) => {
+		return response.json();
+	})
 
-// 	.then((data) => {
-// 		console.log(data);
-// 	});
+	.then((data) => {
+		console.log(data);
+	});
 
-// let NPKey = {
+let NPKey = {
 
-// 	"apiKey": "bf9184305646fc07dfee4e2c2f6826e7",
-// 	"modelName": "InternetDocument",
-// 	"calledMethod": "getDocumentList",
-// 	"methodProperties": {
+	"apiKey": "bf9184305646fc07dfee4e2c2f6826e7",
+	"modelName": "InternetDocument",
+	"calledMethod": "getDocumentList",
+	"methodProperties": {
 
-// 		"DateTimeFrom": "01.01.2020",
-// 		"DateTimeTo": "24.06.2020",
-// 		"Page": "1",
-// 		"GetFullList": "1"
+		"DateTimeFrom": "01.01.2020",
+		"DateTimeTo": "24.06.2020",
+		"Page": "1",
+		"GetFullList": "1"
 
-// 	}
-
-// }
-
-$(function () {
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://api.novaposhta.ua/v2.0/json/",
-		"method": "POST",
-		"headers": {
-			"content-type": "application/json",
-
-		},
-		"processData": false,
-		"data": "{\r\n\"apiKey\": \"\",\r\n \"modelName\": \"Address\",\r\n \"calledMethod\": \"searchSettlements\",\r\n \"methodProperties\": {\r\n \"CityName\": \"васильевка\",\r\n \"Limit\": 5\r\n }\r\n}"
 	}
 
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-	});
-});
+}
