2992f6f0d771f5673e65c72099c83a74b65a4265
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-05-24 19:57:11 +0300

    print EN

--- js/main.js
+++ js/main.js
@@ -11,167 +11,35 @@ const keyObj = {
 	"methodProperties": {
 		"DateTimeFrom": "01.05.2020",
 		"DateTimeTo": "23.05.2020",
-		"Page": "1",
+		// "Page": "1",
 		"GetFullList": "1"
 	}
 }
+//stringify
 function stringifyKeyObj(keyObj) {
 	return JSON.stringify(keyObj);
 }
-async function start(stringifyKeyObj) {
-	try {
-		const response = await fetch(url, {
-			method: 'POST',
-			body: stringifyKeyObj,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		const answer = await response.json();
-		console.log('Успех:', answer);
-	} catch (error) {
-		console.error('Ошибка:', error);
+function consoleOut() {
+	console.log('Успех:', answer);
+	let lengthArray = answer.data.length;
+	for (let i = 0; i > lengthArray; i++) {
+		console.log('Обьект', answer.data[i]);
 	}
 }
-// async function post1() {
-// let body = {
-// 	"apiKey": "bf9184305646fc07dfee4e2c2f6826e7",
-// 	"modelName": "InternetDocument",
-// 	"calledMethod": "getDocumentList",
-// 	"methodProperties": {
-// 		"DateTimeFrom": "01.05.2020",
-// 		"DateTimeTo": "23.05.2020",
-// 		"Page": "1",
-// 		"GetFullList": "1"
-// 	}
-// }
-// console.log(body.apiKey);
+// get all numbers
+async function start(stringifyKeyObj) {
 
-
-// 	let response = await fetch('https://api.novaposhta.ua/v2.0/json/', {
-// 		method: 'POST',
-// 		dataType: 'json',
-// 		body: JSON.stringify(body),
-// 		headers: {
-// 			'Content-Type': 'application/json;charset=utf-8'
-// 		},
-
-// 	});
-
-// 	let result = await response;
-// 	console.log(result);
-
-// }
-// $.ajax({
-// 	type: 'POST',
-// 	dataType: 'json',
-// 	url: 'https://api.novaposhta.ua/v2.0/json/',
-// 	data: JSON.stringify(
-// 		{
-// 			"apiKey": `${key.value}`,
-// 			"modelName": "InternetDocument",
-// 			"calledMethod": "getDocumentList",
-// 			"methodProperties": {
-// 				"DateTimeFrom": "01.05.2020",
-// 				"DateTimeTo": "23.05.2020",
-// 				"Page": "1",
-// 				"GetFullList": "1"
-// 			}
-// 		}
-// 	),
-// 	headers: {
-// 		'Content-Type': 'application/json'
-// 	},
-// 	xhrFields: {
-// 		withCredentials: false
-// 	},
-// 	success: function (texts) {
-// 		console.log(texts);
-// 	},
-// });
-
-// function getsFilms() {
-// 	fetch('https://api.novaposhta.ua/v2.0/json/')
-// 		.then((response) => {
-// 			return response.json();
-// 		})
-// 		.then((data) => {
-// 			console.log(data);
-// 		});
-// }
-// 'use strict'
-// window.addEventListener('load',
-// 	() => {
-// 		pClick.addEventListener("click", getFilms)
-// 		function getFilms() {
-// 			errorOut.style.display = "none";
-// 			let xhr = new XMLHttpRequest();
-// 			xhr.onreadystatechange = () => {
-// 				if (xhr.readyState == XMLHttpRequest.DONE) {
-// 					let films = JSON.parse(xhr.responseText);
-// 					if (films.Response == "False") {
-// 						errorOut.style.display = "block";
-// 					} else {
-// 						let b = '',
-// 							modulText = '';
-// 						films.Search.forEach((film) => {
-// 							// console.log(film);
-// 							let wayImg;
-// 							film.Poster == 'N/A' ? wayImg = 'img/film-3057394.jpg' : wayImg = film.Poster;
-// 							b += `<div>
-// <h2>${film.Title}</h2>
-// <img src="${wayImg}" width="200px" class="img" data-id ="${film.imdbID}" >
-// <h4> Type: ${film.Type}, ${film.Year}</h4>
-// </div>
-// `;
-// 						})
-// 						out.innerHTML = b;
-
-// 					}
-// 				}
-// 			}
-// 			xhr.open('GET', 'https://www.omdbapi.com/?apikey=d7790324&s=' + inputSearch.value + '&&tt0852731', true);
-// 			xhr.send(null);
-// 		}
-// 		document.addEventListener('click', (event) => {
-// 			let tagName = event.target.tagName.toLowerCase();
-// 			let dateName = event.target.dataset.id;
-
-// 			if (tagName == 'img') {
-// 				document.querySelector('.modal').classList.remove('close');
-// 				getUsers(dateName);
-// 			}
-// 			if (tagName == 'span') document.querySelector('.modal').classList.add('close');
-
-// 			// let response = await fetch('https://www.omdbapi.com/?apikey=d7790324&i=' + dateName); // завершается с заголовками ответа
-// 			// let result = await response.json(); // читать тело ответа в формате JSON
-// 			// console.log(result);
-
-// 		});
-// 		function getUsers(dateName) {
-
-// 			let idFilm = new XMLHttpRequest();
-// 			idFilm.onreadystatechange = () => {
-// 				if (idFilm.readyState == XMLHttpRequest.DONE) {
-// 					let filmsId = JSON.parse(idFilm.responseText),
-// 						mod = '';
-// 					mod += `
-// 					<div>
-// 					<span class="close-modal">&#10006;</span>
-// 						<h2>About ${filmsId.Title} :</h2>
-// 						<div>Actors: ${filmsId.Actors}</div>
-// 						<div>Country: ${filmsId.Country}</div>
-// 						<div>Released: ${filmsId.Released}</div>
-// 						<div>Type: ${filmsId.Type}</div>
-// 						<div>Writer: ${filmsId.Writer}</div>
-// 						<div>Year: ${filmsId.Year}</div>
-// 					</div>
-// 					`;
-// 					outModal.innerHTML = mod;
-// 				}
-// 			}
-// 			idFilm.open('GET', 'https://www.omdbapi.com/?apikey=d7790324&i=' + dateName, true);
-// 			idFilm.send(null);
-
-// 		}
-// 	}, false);
+	const response = await fetch(url, {
+		method: 'POST',
+		body: stringifyKeyObj,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	let answerObj = {}
+	const answer = await response.json();
+	// console.log('Успех:', answer);
+	for (let i in answer.data) {
+		console.log('ЕН', answer.data[i].IntDocNumber);
+	}
+}
