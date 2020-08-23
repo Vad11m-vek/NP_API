0dddb6b7f0b7745dd952f65eb0fa8a3fb14925a7
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-08-23 20:04:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const keyObj = {
 	"modelName": "InternetDocument",
 	"calledMethod": "getDocumentList",
 	"methodProperties": {
-		"DateTimeFrom": "01.05.2020",
+		"DateTimeFrom": "01.08.2020",
 		"DateTimeTo": "23.08.2020",
 		"Page": "1",
 		"GetFullList": "1"
